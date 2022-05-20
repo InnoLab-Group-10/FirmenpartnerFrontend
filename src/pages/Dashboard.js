7122038172e843d5178aba66360d55ba0b0f8476
USERNAME <admin@scrysoft-studios.com>
2022-05-20 14:11:39 +0200

    Calendar Component preparation

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import Calendar from 'react-calendar';
 import SiteTitle from '../components/SiteTitle';
+
+import 'react-calendar/dist/Calendar.css';
 
 const Dashboard = () => {
 	return (
@@ -9,6 +12,7 @@ const Dashboard = () => {
 				subtitle='Unterüberschrift zum Dashboard'
 				text='Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam.'
 			/>
+			<Calendar/>
 		</div>
 	);
 };
