89e9f5023fc9fb781add44fb193e2fa11facb374
StephanWeinlinger <stephan.weinlinger@gmail.com>
2022-06-24 17:14:18 +0200

    Add comments to calendar

--- src/components/CalendarComponent.js
+++ src/components/CalendarComponent.js
@@ -1,15 +1,22 @@
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
+
 import 'react-calendar/dist/Calendar.css';
 
 const CalendarComponent = () => {
-  const [value, onChange] = useState(new Date());
+	const [values, setValues] = useState([new Date()]);
+	// const { notifications } = useSelector(state => state.notification);
+	// const { appointments } = useSelector(state => state.timeline);
 
-  return (
-    <>
-      <Calendar onChange={onChange} value={value} />
-    </>
-  );
+	// useEffect(() => {
+	// 	console.log(notifications.concat(appointments));
+	// 	console.log(
+	// 		notifications.concat(appointments).map(entry => new Date(entry.timestamp))
+	// 	);
+	// 	setValues(notifications.concat(appointments).map(entry => new Date(entry.timestamp)));
+	// }, [notifications, appointments]);
+
+	return <Calendar onChange={setValues} defaultValue={values} />;
 };
 
 export default CalendarComponent;
