88388dd0c0c6d189c25ad8684cc21f3e59df64c6
StephanWeinlinger <stephan.weinlinger@gmail.com>
2023-01-10 22:11:22 +0100

    Fix calendar

--- src/components/CalendarComponent.js
+++ src/components/CalendarComponent.js
@@ -1,43 +1,27 @@
 import React from 'react';
 
-import {FullCalendar} from "/@fullcalendar/react";
-import dayGridPlugin from "/@fullcalendar/daygrid";
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
 //import timeGridPlugin from "@fullcalendar/timegrid";
 
-import "@fullcalendar/core/main.css";
-import "@fullcalendar/daygrid/main.css";
-import "@fullcalendar/timegrid/main.css";
-
-import events from "./Events";
+import events from './Events';
 
 const CalendarComponent = () => {
 	return (
-	<>
-	<FullCalendar
-        defaultView="dayGridMonth"
-        header={{
-          left: "prev,next",
-          center: "title",
-          right: "dayGridMonth,timeGridWeek,timeGridDay"
-        }}
-        themeSystem="Simplex"
-        plugins={[dayGridPlugin]}
-        events={events}
-      />
-      <FullCalendar
-        defaultView="dayGridMonth"
-        // themeSystem="Simplex"
-        // header={{
-        //   left: "prev,next",
-        //   center: "title",
-        //   right: "dayGridMonth,timeGridWeek,timeGridDay",
-        // }}
-        plugins={[dayGridPlugin]}
-        events={events}
-        displayEventEnd="true"
-        eventColor={"#" + Math.floor(Math.random() * 16777215).toString(16)}
-      />
-	</>)
+		<>
+			<FullCalendar
+				defaultView='dayGridMonth'
+				header={{
+					left: 'prev,next',
+					center: 'title',
+					right: 'dayGridMonth,timeGridWeek,timeGridDay',
+				}}
+				themeSystem='Simplex'
+				plugins={[dayGridPlugin]}
+				events={events}
+			/>
+		</>
+	);
 };
 
 export default CalendarComponent;
