2a80dbf5ce2f4e004409c8346ed95d57c61dff79
StephanWeinlinger <stephan.weinlinger@gmail.com>
2023-01-12 15:50:40 +0100

    Add calendar events

--- src/components/CalendarComponent.js
+++ src/components/CalendarComponent.js
@@ -1,44 +1,63 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
-
+import { useSelector } from 'react-redux';
 
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
-
-import events from './Events';
+import timegridPlugin from '@fullcalendar/timegrid';
 
 const CalendarComponent = () => {
+	const { appointments } = useSelector(state => state.timeline);
+	const { notifications } = useSelector(state => state.notification);
+
+	const handleClick = e => {
+		const event = e.event;
+		if (event.url) {
+			alert(
+				event.title +
+					'\n' +
+					'Öffnet den Link: ' +
+					event.url +
+					' in einem neuen Browsertab.'
+			);
+
+			window.open(event.url);
+
+			e.jsEvent.preventDefault(); // prevents browser from following link in current tab.
+		} else {
+			alert(event.title);
+		}
+	};
+
 	return (
 		<Card>
-			<Card.Body className="calendar-styling-override">
+			<Card.Body className='calendar-styling-override'>
 				<FullCalendar
-					defaultView='dayGridMonth'
-					header={{
+					initialView='dayGridMonth'
+					headerToolbar={{
 						left: 'prev,next',
 						center: 'title',
-						right: 'dayGridMonth,timeGridWeek,timeGridDay',
+						right: 'dayGridMonth, timeGridWeek, timeGridDay',
 					}}
 					weekNumbers='true'
 					contentHeight={650}
-					plugins={[dayGridPlugin]}
-					events={events}
-					eventClick = {function(info) {
-						var eventObj = info.event;
-				  
-						if (eventObj.url) {
-						  alert(
-							eventObj.title + '\n' +
-							'Öffnet den Link: ' + eventObj.url + ' in einem neuen Browsertab.'
-						  );
-				  
-						  window.open(eventObj.url);
-				  
-						  info.jsEvent.preventDefault(); // prevents browser from following link in current tab.
-						} else {
-						  alert(eventObj.title);
-						}
-					  }
-					}
+					plugins={[dayGridPlugin, timegridPlugin]}
+					events={[
+						...appointments.map(entry => ({
+							id: entry.id,
+							title: entry.message,
+							start: entry.timestamp,
+							url: entry.link ? entry.link : '',
+							backgroundColor: 'blue',
+						})),
+						...notifications.map(entry => ({
+							id: entry.id,
+							title: entry.message,
+							start: entry.timestamp,
+							backgroundColor: 'red',
+						})),
+					]}
+					eventClick={handleClick}
 				/>
 			</Card.Body>
 		</Card>
