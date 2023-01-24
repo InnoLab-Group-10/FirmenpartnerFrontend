5aad381cf2b6b67b3050cf097675823257021685
StephanWeinlinger <stephan.weinlinger@gmail.com>
2023-01-24 21:23:21 +0100

    Fix company row formatting

--- src/components/tables/PartnerRow.js
+++ src/components/tables/PartnerRow.js
@@ -69,122 +69,120 @@ const PartnerRow = props => {
 				<tr id='example-collapse-text' className='detailed-row'>
 					<td colSpan={5}>
 						<Table>
-							<tr>
-								<td>
-									<Table>
-										<tbody>
-											<tr>
-												<td>
-													<strong>Anschrift:</strong>
-												</td>
-												<td>
-													{locations.length !== 0
-												? `${locations[0].address}, ${locations[0].zipcode}, ${locations[0].city}`
-												: 'N/A, N/A, N/A'}
-												</td>
-											</tr>
-											<tr>
-												<td>
-													<strong>Studierende:</strong>
-												</td>
-												<td>
-													{company.maxStudents} pro Jahr
-												</td>
-											</tr>
-											<tr>
-												<td>
-													<strong>Notiz:</strong>
-												</td>
-												<td>
-													{company.notes}
-												</td>
-											</tr>									
-										</tbody>
-									</Table>
-								</td>
-							</tr>
-							<tr>
-								<td>
-									<Table>
-										<thead>
-											<tr>
-												<td><strong>AnsprechpartnerIn</strong></td>
-												<td><strong>E-Mail</strong></td>
-												<td><strong>Telefonnummer</strong></td>
-												<td>
-													<Button variant='secondary' className='table-icons' onClick={toggleEditShow}>
-														<BiPlus />
-													</Button>
-												</td>
-											</tr>
-										</thead>
-										<tbody>
-											<tr>
-												<td>
-													Vorname Nachname
-												</td>
-												<td>
-													text@test.test
-												</td>
-												<td>
-													+43 (0) 000 000 00 00
-												</td>
-												<td>
-													<Button
-														variant='danger'
-														className='table-icons table-delete-icon'
-														onClick={toggleDeleteShow}
-													>
-														<BiTrash />
-													</Button>
-													<Button variant='secondary' className='table-icons' onClick={toggleEditShow}>
-														<BiPencil />
-													</Button>
-												</td>
-											</tr>
-										</tbody>
-									</Table>
-								</td>
-							</tr>
-							<tr>
-								<td>
-									<Table>
-										<thead>
-											<tr>
-												<td><strong>Jahr</strong></td>
-												<td><strong>Aufgenommene Studierende</strong></td>
-												<td>
-													<Button variant='secondary' className='table-icons' onClick={toggleEditShow}>
-														<BiPlus />
-													</Button>
-												</td>
-											</tr>
-										</thead>
-										<tbody>
-											<tr>
-												<td>
-													2018
-												</td>
-												<td>
-													99
-												</td>
-												<td>
-													<Button
-														variant='danger'
-														className='table-icons table-delete-icon'
-														onClick={toggleDeleteShow}
-													>
-														<BiTrash />
-													</Button>
-													<Button variant='secondary' className='table-icons' onClick={toggleEditShow}>
-														<BiPencil />
-													</Button>
-												</td>
-											</tr>
-										</tbody>
-									</Table>
-								</td>
-							</tr>
+							<tbody>
+								<tr>
+									<td>
+										<strong>Anschrift:</strong>
+									</td>
+									<td>
+										{locations.length !== 0
+											? `${locations[0].address}, ${locations[0].zipcode}, ${locations[0].city}`
+											: 'N/A, N/A, N/A'}
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<strong>Studierende:</strong>
+									</td>
+									<td>{company.maxStudents} pro Jahr</td>
+								</tr>
+								<tr>
+									<td>
+										<strong>Notiz:</strong>
+									</td>
+									<td>{company.notes}</td>
+								</tr>
+							</tbody>
+						</Table>
+						<Table>
+							<thead>
+								<tr>
+									<td>
+										<strong>AnsprechpartnerIn</strong>
+									</td>
+									<td>
+										<strong>E-Mail</strong>
+									</td>
+									<td>
+										<strong>Telefonnummer</strong>
+									</td>
+									<td>
+										<Button
+											variant='secondary'
+											className='table-icons'
+											onClick={toggleEditShow}
+										>
+											<BiPlus />
+										</Button>
+									</td>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>Vorname Nachname</td>
+									<td>text@test.test</td>
+									<td>+43 (0) 000 000 00 00</td>
+									<td>
+										<Button
+											variant='danger'
+											className='table-icons table-delete-icon'
+											onClick={toggleDeleteShow}
+										>
+											<BiTrash />
+										</Button>
+										<Button
+											variant='secondary'
+											className='table-icons'
+											onClick={toggleEditShow}
+										>
+											<BiPencil />
+										</Button>
+									</td>
+								</tr>
+							</tbody>
+						</Table>
+						<Table>
+							<thead>
+								<tr>
+									<td>
+										<strong>Jahr</strong>
+									</td>
+									<td>
+										<strong>Aufgenommene Studierende</strong>
+									</td>
+									<td>
+										<Button
+											variant='secondary'
+											className='table-icons'
+											onClick={toggleEditShow}
+										>
+											<BiPlus />
+										</Button>
+									</td>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>2018</td>
+									<td>99</td>
+									<td>
+										<Button
+											variant='danger'
+											className='table-icons table-delete-icon'
+											onClick={toggleDeleteShow}
+										>
+											<BiTrash />
+										</Button>
+										<Button
+											variant='secondary'
+											className='table-icons'
+											onClick={toggleEditShow}
+										>
+											<BiPencil />
+										</Button>
+									</td>
+								</tr>
+							</tbody>
 						</Table>
 					</td>
 				</tr>
@@ -221,18 +219,14 @@ const PartnerRow = props => {
 				<Modal.Header closeButton>
 					<Modal.Title>Ansprechperson hinzufügen</Modal.Title>
 				</Modal.Header>
-				<Modal.Body>
-					Formular Vorname, Nachname, Email, Nummer
-				</Modal.Body>
+				<Modal.Body>Formular Vorname, Nachname, Email, Nummer</Modal.Body>
 			</Modal>
 			{/* Add Students Per Year Modal */}
 			<Modal size='xl' show={showEdit} onHide={toggleEditShow} keyboard={false}>
 				<Modal.Header closeButton>
 					<Modal.Title>Studierende pro Jahr</Modal.Title>
 				</Modal.Header>
-				<Modal.Body>
-					Jahr, Anzahl
-				</Modal.Body>
+				<Modal.Body>Jahr, Anzahl</Modal.Body>
 			</Modal>
 		</>
 	);
